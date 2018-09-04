14310161c3b681e7cd1c0b2883132814301f614b
Byeongman Lee <ssey0921@gmail.com>
2018-09-05 01:56:56 +0900

    practice에 로그인 기능 추가

--- practice/server.js
+++ practice/server.js
@@ -2,11 +2,11 @@ var express = require('express');
 var app = express();
 var router = require('./router/main')(app);
 
-app.set('views', __dirname +'/views');
+app.set('views', __dirname + '/views');
 app.set('view engine', 'ejs');
 app.engine('html', require('ejs').renderFile);
 
-var server = app.listen(3000, function(){
+var server = app.listen(3000, function () {
     console.log("3000 port open")
 });
 
